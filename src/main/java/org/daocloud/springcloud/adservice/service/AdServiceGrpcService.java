2bcb7d04eeb27e49edb37ae430182244d5c26d04
minquan.chen <minquan.chen@daocloud.io>
2022-10-29 18:11:48 +0800

    update

--- src/main/java/org/daocloud/springcloud/adservice/service/AdServiceGrpcService.java
+++ src/main/java/org/daocloud/springcloud/adservice/service/AdServiceGrpcService.java
@@ -33,7 +33,7 @@ public class AdServiceGrpcService extends AdServiceGrpc.AdServiceImplBase {
     private static final Logger logger = LogManager.getLogger(AdServiceGrpcService.class);
     private static final ImmutableListMultimap<String, Ad> adsMap = createAdsMap();
 
-    private static final String SINGALTEXT =" -- by a adservice registered in nacos";
+    private static final String SINGALTEXT =" -- by a new impl adservice";
     @Override
     public void getAds(AdRequest req, StreamObserver<AdResponse> responseObserver) {
 
@@ -106,48 +106,47 @@ public class AdServiceGrpcService extends AdServiceGrpc.AdServiceImplBase {
         return ads;
     }
     private static ImmutableListMultimap<String, Ad> createAdsMap() {
-        Ad hairdryer =
+        Ad binoculars =
                 Ad.newBuilder()
                         .setRedirectUrl("/product/2ZYFJ3GM2N")
-                        .setText("Hairdryer for sale. 50% off."+SINGALTEXT)
+                        .setText("Roof Binoculars for sale. 50% off."+ SINGALTEXT)
                         .build();
-        Ad tankTop =
+        Ad explorerTelescope =
                 Ad.newBuilder()
                         .setRedirectUrl("/product/66VCHSJNUP")
-                        .setText("Tank top for sale. 20% off."+SINGALTEXT)
+                        .setText("Starsense Explorer Refractor Telescope for sale. 20% off."+ SINGALTEXT)
                         .build();
-        Ad candleHolder =
+        Ad colorImager =
                 Ad.newBuilder()
                         .setRedirectUrl("/product/0PUK6V6EV0")
-                        .setText("Candle holder for sale. 30% off."+SINGALTEXT)
+                        .setText("Solar System Color Imager for sale. 30% off."+ SINGALTEXT)
                         .build();
-        Ad bambooGlassJar =
+        Ad opticalTube =
                 Ad.newBuilder()
                         .setRedirectUrl("/product/9SIQT8TOJO")
-                        .setText("Bamboo glass jar for sale. 10% off."+SINGALTEXT)
+                        .setText("Optical Tube Assembly for sale. 10% off."+ SINGALTEXT)
                         .build();
-        Ad watch =
+        Ad travelTelescope =
                 Ad.newBuilder()
                         .setRedirectUrl("/product/1YMWWN1N4O")
-                        .setText("Watch for sale. Buy one, get second kit for free"+SINGALTEXT)
+                        .setText("Eclipsmart Travel Refractor Telescope for sale. Buy one, get second kit for free"+ SINGALTEXT)
                         .build();
-        Ad mug =
+        Ad solarFilter =
                 Ad.newBuilder()
                         .setRedirectUrl("/product/6E92ZMYYFZ")
-                        .setText("Mug for sale. Buy two, get third one for free"+SINGALTEXT)
+                        .setText("Solar Filter for sale. Buy two, get third one for free"+ SINGALTEXT)
                         .build();
-        Ad loafers =
+        Ad cleaningKit =
                 Ad.newBuilder()
                         .setRedirectUrl("/product/L9ECAV7KIM")
-                        .setText("Loafers for sale. Buy one, get second one for free"+SINGALTEXT)
+                        .setText("Lens Cleaning Kit for sale. Buy one, get second one for free"+ SINGALTEXT)
                         .build();
         return ImmutableListMultimap.<String, Ad>builder()
-                .putAll("clothing", tankTop)
-                .putAll("accessories", watch)
-                .putAll("footwear", loafers)
-                .putAll("hair", hairdryer)
-                .putAll("decor", candleHolder)
-                .putAll("kitchen", bambooGlassJar, mug)
+                .putAll("binoculars", binoculars)
+                .putAll("telescopes", explorerTelescope)
+                .putAll("accessories", colorImager, solarFilter, cleaningKit)
+                .putAll("assembly", opticalTube)
+                .putAll("travel", travelTelescope)
                 .build();
     }
 }
