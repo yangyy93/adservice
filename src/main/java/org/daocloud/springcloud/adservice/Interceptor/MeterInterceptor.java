a0b518eb608c1ae079e62aba41d9f6a349e842fe
minquan.chen <minquan.chen@daocloud.io>
2022-11-23 13:32:51 +0800

    add a failed label for grpc_calls_total

--- src/main/java/org/daocloud/springcloud/adservice/Interceptor/MeterInterceptor.java
+++ src/main/java/org/daocloud/springcloud/adservice/Interceptor/MeterInterceptor.java
@@ -7,16 +7,24 @@ import org.apache.logging.log4j.Logger;
 import org.daocloud.springcloud.adservice.meter.Meter;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static io.grpc.Metadata.ASCII_STRING_MARSHALLER;
+
 public class MeterInterceptor implements ServerInterceptor {
     @Autowired
     private Meter meterProvider;
+
+    public static final Context.Key<Object> operation
+            = Context.key("operation");
 
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> serverCall, Metadata metadata, ServerCallHandler<ReqT, RespT> serverCallHandler) {
         long start = System.currentTimeMillis();
         Attributes attributes = Attributes.of(AttributeKey.stringKey("operation"), serverCall.getMethodDescriptor().getFullMethodName());
         meterProvider.getGrpcCalls().add(1,attributes);
-        return serverCallHandler.startCall(
+        Context context = Context.current().withValue(operation, serverCall.getMethodDescriptor().getFullMethodName());
+
+        ServerCall.Listener<ReqT> listener = Contexts.interceptCall(
+                context,
                 new ForwardingServerCall.SimpleForwardingServerCall<>(serverCall) {
                     @Override
                     public void sendMessage(RespT message) {
@@ -26,6 +34,9 @@ public class MeterInterceptor implements ServerInterceptor {
                         super.sendMessage(message);
                     }
                 },
-                metadata);
+                metadata,
+                serverCallHandler);
+
+        return listener;
     }
 }
